8723274ce232b8b85a72a76e807d34c9cf0a5a44
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-03-22 04:20:18 +0200

    switch to fresh nextjs frontend

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -316,7 +316,7 @@ module.exports = {
     },
   },
   abiExporter: {
-    path: "../react-app/src/contracts/ABI",
+    path: "../frontend/utils/contracts/ABI",
     runOnCompile: true,
     clear: true,
     flat: true,
