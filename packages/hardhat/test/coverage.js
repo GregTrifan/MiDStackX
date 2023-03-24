57e93058a3ef967392cd2e8480988966e7cf6fef
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-03-24 05:10:27 +0200

    add tests

--- packages/hardhat/test/coverage.js
+++ packages/hardhat/test/coverage.js
@@ -25,5 +25,23 @@ describe("StackXOwner", function() {
         creator.address
       );
     });
+    it("Fan should be able to mint with custom priced NFT", async function() {
+      const [creator, fan, creator2] = await ethers.getSigners();
+      // Change price for the creator2 NFT
+      await stackXOwner
+        .connect(creator2)
+        .setCreatorPrice(ethers.utils.parseEther("0.1"));
+      // Mint the NFT with the custom amount
+      await stackXOwner
+        .connect(fan)
+        .mint(creator2.address, { value: ethers.utils.parseEther("0.1") });
+      // Check if the second NFT is minted
+      expect(await stackXOwner.getTokenAddress(2)).to.equal(creator2.address);
+      // Ensure there are both creators here
+      expect(await stackXOwner.connect(fan).getOwnedCreators()).to.contain(
+        creator.address,
+        creator2.address
+      );
+    });
   });
 });
